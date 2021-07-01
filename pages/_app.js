431a4fff4b60035868b4e84c769fd6fd24413cc5
Mateusz Masłowiec <mateusz.maslowiec98@gmail.com>
2021-07-01 21:02:09 +0200

    Setup material-ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,32 @@
+import React, { useEffect } from "react";
 import "../styles/globals.css";
-
+import PropTypes from "prop-types";
+import { ThemeProvider } from "@material-ui/styles";
+import { CssBaseline } from "@material-ui/core";
 import { wrapper } from "../redux/store";
+import Layout from "../components/Layout/Layout";
+import theme from "../components/theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Layout {...pageProps}>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  );
 }
 
 export default wrapper.withRedux(MyApp);
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
