62cbba35c0c49d45951524def9913cdf8a7930b1
Mateusz Masłowiec <mateusz.maslowiec98@gmail.com>
2021-07-01 21:46:37 +0200

    Create login page

---
+++ pages/login.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  TextField,
+} from "@material-ui/core";
+import { useDispatch } from "react-redux";
+
+const useStyles = makeStyles((theme) => ({
+  loginContainer: {
+    width: "45%",
+    height: "60vh",
+    background: "rgba( 255, 255, 255, 0.25 )",
+    boxShadow: "0 2px 12px 0 rgba( 255, 255, 255, 0.2 )",
+    backdropFilter: "blur( 10.0px )",
+    border: "1px solid rgba( 255, 255, 255, 0.18 )",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    zIndex: "999",
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+}));
+
+const Login = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login(values.email, values.password));
+  };
+  return (
+    <Card className={classes.loginContainer}>
+      <h1 style={{ marginLeft: "20px" }}>Login</h1>
+      <CardContent>
+        <TextField
+          id="email"
+          type="email"
+          label="Email"
+          className={classes.textField}
+          value={values.email}
+          onChange={handleChange("email")}
+          margin="normal"
+          variant="outlined"
+        />
+        <br />
+        <TextField
+          id="password"
+          type="password"
+          label="Password"
+          className={classes.textField}
+          value={values.password}
+          onChange={handleChange("password")}
+          margin="normal"
+          variant="outlined"
+        />
+        <CardActions style={{ padding: "8px 0px" }}>
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={handleSubmit}
+            className={classes.submit}
+          >
+            Submit
+          </Button>
+        </CardActions>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Login;
