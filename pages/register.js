e4e64136a90a5b59f04e1892482cc2fbc2d4c1ed
Mateusz Masłowiec <mateusz.maslowiec98@gmail.com>
2021-07-01 21:43:16 +0200

    Create register page

--- pages/register.js
+++ pages/register.js
@@ -1,9 +1,117 @@
-import React from "react";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  TextField,
+} from "@material-ui/core";
+import { useDispatch } from "react-redux";
 
-const register = () => (
-  <div>
-    <h1>Register</h1>
-  </div>
-);
+const useStyles = makeStyles((theme) => ({
+  registerContainer: {
+    width: "45%",
+    height: "60vh",
+    background: "rgba( 255, 255, 255, 0.25 )",
+    boxShadow: "0 2px 12px 0 rgba( 255, 255, 255, 0.2 )",
+    backdropFilter: "blur( 10.0px )",
+    border: "1px solid rgba( 255, 255, 255, 0.18 )",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    zIndex: "999",
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+}));
+
+const register = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(
+      register(
+        values.name,
+        values.email,
+        values.password,
+        values.passwordConfirm,
+      ),
+    );
+  };
+  return (
+    <Card className={classes.registerContainer}>
+      <h1 style={{ marginLeft: "20px" }}>Register</h1>
+      <CardContent>
+        <TextField
+          id="name"
+          label="Name"
+          className={classes.textField}
+          value={values.name}
+          onChange={handleChange("name")}
+          margin="normal"
+          variant="outlined"
+        />
+        <br />
+        <TextField
+          id="email"
+          type="email"
+          label="Email"
+          className={classes.textField}
+          value={values.email}
+          onChange={handleChange("email")}
+          margin="normal"
+          variant="outlined"
+        />
+        <br />
+        <TextField
+          id="password"
+          type="password"
+          label="Password"
+          className={classes.textField}
+          value={values.password}
+          onChange={handleChange("password")}
+          margin="normal"
+          variant="outlined"
+        />
+        <br />
+        <TextField
+          id="passwordConfirm"
+          type="password"
+          label="Password Confirm"
+          className={classes.textField}
+          value={values.passwordConfirm}
+          onChange={handleChange("passwordConfirm")}
+          margin="normal"
+          variant="outlined"
+        />
+        <CardActions style={{ padding: "8px 0px" }}>
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={handleSubmit}
+            className={classes.submit}
+          >
+            Submit
+          </Button>
+        </CardActions>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default register;
