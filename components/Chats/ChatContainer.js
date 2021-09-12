e2585dc7c31932af2e0344c076692808351b870e
M4asl <mateusz.maslowiec98@gmail.com>
2021-09-12 15:07:32 +0200

    Add loader for no data display

--- components/Chats/ChatContainer.js
+++ components/Chats/ChatContainer.js
@@ -1,9 +1,11 @@
 import { makeStyles } from "@material-ui/core";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import ChatHeader from "./ChatHeader";
 import ChatInputs from "./ChatInputs";
 import ChatMessages from "./ChatMessages";
+import Loader from "../Layout/Loader";
 
 const useStyles = makeStyles((theme) => ({
   chatContainer: {
@@ -11,20 +13,32 @@ const useStyles = makeStyles((theme) => ({
     height: "calc(100vh - 66px)",
     flexDirection: "column",
     justifyContent: "space-between",
+    position: "relative",
   },
 }));
 
 const Chat = ({ widthProps }) => {
   const classes = useStyles();
+  const { chatReducer } = useSelector((state) => state);
+
+  const { chat } = chatReducer;
 
   return (
     <div
       style={{ width: `${widthProps}` }}
       className={classes.chatContainer}
     >
-      <ChatHeader />
-      <ChatMessages />
-      <ChatInputs />
+      {chat &&
+      Object.keys(chat).length > 0 &&
+      chat.constructor === Object ? (
+        <>
+          <ChatHeader />
+          <ChatMessages />
+          <ChatInputs />
+        </>
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 };
