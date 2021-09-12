e064997beb842d56afa51fe1372b7a9a74f017b1
M4asl <mateusz.maslowiec98@gmail.com>
2021-09-12 15:08:35 +0200

    Add class for active chat and add links

--- components/Chats/ChatList.js
+++ components/Chats/ChatList.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import Link from "next/link";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
@@ -10,8 +11,13 @@ import { useRouter } from "next/router";
 import { AvatarGroup } from "@material-ui/lab";
 
 const useStyles = makeStyles((theme) => ({
-  backgroundChat: {
+  backgroundChatUnactive: {
     background: theme.palette.background.secondary,
+    borderRadius: "15px",
+    margin: "12px 0px",
+  },
+  backgroundChatActive: {
+    background: theme.palette.background.default,
     borderRadius: "15px",
     margin: "12px 0px",
   },
@@ -27,44 +33,57 @@ const ChatList = () => {
   const router = useRouter();
   const { chatReducer, userReducer } = useSelector((state) => state);
   const [chats, setChats] = useState(chatReducer.chats);
-  const [chatId, setChatId] = useState("");
+  const [chatId, setChatId] = useState(router.query.id);
   const currentUserId = userReducer.currentUserDetails._id;
+
+  const handleClick = (index) => {
+    setChatId(index);
+  };
 
   return (
     <List>
       {chats.map((chat) => (
-        <ListItem className={classes.backgroundChat} key={chat._id}>
-          {chat.isGroupChat ? (
-            <ListItemAvatar style={{ marginRight: "16px" }}>
-              <AvatarGroup max={2} spacing="small">
-                {chat.users
-                  .filter((user) => user._id !== currentUserId)
-                  .map((user) => (
-                    <Avatar key={user._id} />
-                  ))}
-              </AvatarGroup>
-            </ListItemAvatar>
-          ) : (
-            <ListItemAvatar>
-              <Avatar />
-            </ListItemAvatar>
-          )}
-          <div className={classes.textBox}>
-            {chat.users
-              .slice(0, 2)
-              .filter((user) => user._id !== currentUserId)
-              .map((user, i) => (
-                <Typography variant="subtitle2" key={user._id}>
-                  {(i ? ", " : "") + user.name}
-                </Typography>
-              ))}
-            {chat.users.length > 3 && (
-              <Typography variant="subtitle2">
-                + {chat.users.length - 3} other users
-              </Typography>
+        <Link href={`/chats/${chat._id}`} key={chat._id}>
+          <ListItem
+            onClick={() => handleClick(chat._id)}
+            className={
+              chatId === chat._id
+                ? classes.backgroundChatActive
+                : classes.backgroundChatUnactive
+            }
+          >
+            {chat.isGroupChat ? (
+              <ListItemAvatar style={{ marginRight: "16px" }}>
+                <AvatarGroup max={2} spacing="small">
+                  {chat.users
+                    .filter((user) => user._id !== currentUserId)
+                    .map((user) => (
+                      <Avatar key={user._id} />
+                    ))}
+                </AvatarGroup>
+              </ListItemAvatar>
+            ) : (
+              <ListItemAvatar>
+                <Avatar />
+              </ListItemAvatar>
             )}
-          </div>
-        </ListItem>
+            <div className={classes.textBox}>
+              {chat.users
+                .slice(0, 2)
+                .filter((user) => user._id !== currentUserId)
+                .map((user, i) => (
+                  <Typography variant="subtitle2" key={user._id}>
+                    {(i ? ", " : "") + user.name}
+                  </Typography>
+                ))}
+              {chat.users.length > 3 && (
+                <Typography variant="subtitle2">
+                  + {chat.users.length - 3} other users
+                </Typography>
+              )}
+            </div>
+          </ListItem>
+        </Link>
       ))}
     </List>
   );
