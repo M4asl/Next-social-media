9bbafaced3b7e9202ae6ae9158a00fcf6549383d
M4asl <mateusz.maslowiec98@gmail.com>
2021-10-18 19:23:25 +0200

    Not creating chat only with current user and add more filed after response data

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -17,6 +17,33 @@ exports.createChat = catchAsync(async (req, res, next) => {
     return next(new AppError("Must add minimum one person.", 400));
   }
 
+  if (
+    users.length == 1 &&
+    req.body.users.toString() == req.user._id
+  ) {
+    return next(
+      new AppError(
+        "You can't create chat beacuse you added your profile.",
+        400,
+      ),
+    );
+  }
+
+  if (users.length > 1) {
+    const currentUser = req.body.users.find(
+      (user) => user == req.user._id,
+    );
+
+    if (currentUser) {
+      return next(
+        new AppError(
+          "You can't create chat beacuse you added your profile.",
+          400,
+        ),
+      );
+    }
+  }
+
   users.push(req.user);
 
   // if chat exist return this chat or create new
@@ -37,13 +64,20 @@ exports.createChat = catchAsync(async (req, res, next) => {
 });
 
 exports.getChatsUser = catchAsync(async (req, res, next) => {
-  const chatUser = await Chat.find({
+  let chatsUser = await Chat.find({
     users: { $elemMatch: { $eq: req.user._id } },
   })
     .populate("users", "_id name photo")
-    .sort({ updatedAt: -1 });
+    .populate("latestMessage")
+    .sort({ updatedAt: -1 })
+    .exec();
 
-  res.status(200).json(chatUser);
+  chatsUser = await User.populate(chatsUser, {
+    path: "latestMessage.sender",
+    select: "_id name",
+  });
+
+  res.status(200).json(chatsUser);
 });
 
 exports.getChatById = catchAsync(async (req, res, next) => {
@@ -60,10 +94,18 @@ exports.getChatById = catchAsync(async (req, res, next) => {
     );
   }
 
-  const chat = await Chat.findOne({
+  let chat = await Chat.findOne({
     _id: chatId,
     users: { $elemMatch: { $eq: userId } },
-  }).populate("users", "_id name photo");
+  })
+    .populate("users", "_id name photo")
+    .populate("latestMessage")
+    .exec();
+
+  chat = await User.populate(chat, {
+    path: "latestMessage.readBy",
+    select: "_id name",
+  });
 
   if (chat == null) {
     const userByChatId = await User.findById(chatId);
